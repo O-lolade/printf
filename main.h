04bdd403c6bfaa5d60124cbea627d19a5ebd32d3
O-lolade <olushesim@gmail.com>
2023-10-16 04:17:14 -0700

    updated

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H
+#
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -8,15 +8,15 @@
 #define BUFF_SIZE 1024
 
 /* FLAGS */
+#define S_LONG 2
+#define S_SHORT 1
+
+/* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
-
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
 
 /**
  * struct fmt - Struct op
